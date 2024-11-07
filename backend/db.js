62c7989eb282981fc9ee1b4cbe1c0648014923dc
martinezmario02 <e.martinezmario@go.ugr.es>
2024-11-07 15:55:46 +0100

    últimos arreglos

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  user: 'mariocc',    
+  password: 'mariocc',
+  database: 'starsbar' 
 });
 
 connection.connect((err) => {
