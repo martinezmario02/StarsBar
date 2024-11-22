b4f8927a107cc2cb8c23f675c94c62b473ea032d
martinezmario02 <e.martinezmario@go.ugr.es>
2024-11-22 16:38:15 +0100

    hito finalizado

--- backend/db.js
+++ backend/db.js
@@ -1,31 +1,3 @@
-// const mysql = require('mysql2');
-// const dotenv = require('dotenv');
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'mariocc',    
-//   password: 'mariocc',
-//   database: 'starsbar' 
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error conectando a la base de datos:', err);
-//     return;
-//   }
-//   console.log('Conexión a la base de datos establecida');
-// });
-
-// function close() {
-//   if (connection) {
-//       connection.end((err) => {
-//           if (err) throw err;
-//           console.log('Conexión a la base de datos cerrada');
-//       });
-//   }
-// }
-// module.exports = connection; 
-
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
